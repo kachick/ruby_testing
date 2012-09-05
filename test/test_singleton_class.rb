7f8cdcc0193af4a572d985677a941209b3b7228b
Kenichi Kamiya <kachick1+github@gmail.com>
2012-09-05 14:30:08 +0900

    add

--- test/test_singleton_class.rb
+++ test/test_singleton_class.rb
@@ -6,26 +6,38 @@ class Foo
 
   class << self
     
+    def public_first
+      :public_first
+    end
+    
     private
 
-    def foo
-      :foo
+    def private_first
+      :private_first
     end
     
     class << self
       
+      def public_second
+        :public_second
+      end
+      
       private
       
-      def bar
-        :bar
+      def private_second
+        :private_second
       end
       
       class << self
         
+        def public_too_deep
+          :public_too_deep
+        end
+        
         private
         
-        def hoge
-          :hoge
+        def private_too_deep
+          :private_too_deep
         end
         
       end
@@ -42,25 +54,37 @@ end
 
 class Test_Singleton_Class < Test::Unit::TestCase
   
-  #~ def test_singleton_class_loop
-    #~ assert_same :foo, Foo.foo
-    #~ assert_same :bar, Foo.singleton_class.bar
-    #~ assert_same :hoge, Foo.singleton_class.singleton_class.hoge
-  #~ end
-  
-  def test_singleton_class_loop_via_send
-    assert_same :foo, Foo.__send__(:foo)
-    assert_same :bar, Foo.singleton_class.__send__(:bar)
-    assert_same :hoge, Foo.singleton_class.singleton_class.__send__(:hoge)
+  def test_singleton_class_loop
+    assert_same :public_first, Foo.public_first
+    assert_same :public_second, Foo.singleton_class.public_second
+    assert_same :public_too_deep, Foo.singleton_class.singleton_class.public_too_deep
   end
   
-  def test_singleton_class_inherited_eval
-    assert_same :foo, Bar.__send__(:foo)
+  def test_singleton_class_loop_private_via_send
+    assert_same :private_first, Foo.__send__(:private_first)
+    assert_same :private_second, Foo.singleton_class.__send__(:private_second)
+    assert_same :private_too_deep, Foo.singleton_class.singleton_class.__send__(:private_too_deep)
+  end
+  
+  def test_singleton_class_inherited
+    assert_same :public_first, Bar.public_first
     
     tester = self
     Bar.class_eval do
-      tester.assert_same :bar, singleton_class.__send__(:bar)
-      tester.assert_same :hoge, singleton_class.singleton_class.__send__(:hoge)
+      tester.assert_same :public_second, singleton_class.public_second
+      tester.assert_same :public_too_deep, singleton_class.singleton_class.public_too_deep
+    end
+  end
+  
+  def test_singleton_class_inherited_private_via_send
+    assert_same :private_first, Bar.__send__(:private_first)
+    assert_same :private_second, Bar.singleton_class.__send__(:private_second)
+    assert_same :private_too_deep, Bar.singleton_class.singleton_class.__send__(:private_too_deep)
+    
+    tester = self
+    Bar.class_eval do
+      tester.assert_same :private_second, singleton_class.__send__(:private_second)
+      tester.assert_same :private_too_deep, singleton_class.singleton_class.__send__(:private_too_deep)
     end
   end
 
