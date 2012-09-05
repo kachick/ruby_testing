3f9a040b66bc6d73c2401d39323436df14582b0e
Kenichi Kamiya <kachick1+github@gmail.com>
2012-09-05 14:09:59 +0900

    add via send

--- test/test_singleton_class.rb
+++ test/test_singleton_class.rb
@@ -5,6 +5,8 @@ require 'test/unit'
 class Foo
 
   class << self
+    
+    private
 
     def foo
       :foo
@@ -12,11 +14,15 @@ class Foo
     
     class << self
       
+      private
+      
       def bar
         :bar
       end
       
       class << self
+        
+        private
         
         def hoge
           :hoge
@@ -30,14 +36,17 @@ class Foo
   
 end
 
+class Bar < Foo
+end
+
 
 class Test_Singleton_Class < Test::Unit::TestCase
   
-  def test_singleton_class_loop
-    assert_same :foo, Foo.foo
-    assert_same :bar, Foo.singleton_class.bar
-    assert_same :hoge, Foo.singleton_class.singleton_class.hoge
-  end
+  #~ def test_singleton_class_loop
+    #~ assert_same :foo, Foo.foo
+    #~ assert_same :bar, Foo.singleton_class.bar
+    #~ assert_same :hoge, Foo.singleton_class.singleton_class.hoge
+  #~ end
   
   def test_singleton_class_loop_via_send
     assert_same :foo, Foo.__send__(:foo)
@@ -45,4 +54,14 @@ class Test_Singleton_Class < Test::Unit::TestCase
     assert_same :hoge, Foo.singleton_class.singleton_class.__send__(:hoge)
   end
   
+  def test_singleton_class_inherited_eval
+    assert_same :foo, Bar.__send__(:foo)
+    
+    tester = self
+    Bar.class_eval do
+      tester.assert_same :bar, singleton_class.__send__(:bar)
+      tester.assert_same :hoge, singleton_class.singleton_class.__send__(:hoge)
+    end
+  end
+
 end
