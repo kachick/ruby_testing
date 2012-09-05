2643abfa89c3200e02cc6507e4fe4d4ee7e697fc
Kenichi Kamiya <kachick1+github@gmail.com>
2012-09-05 15:34:36 +0900

    add simple case

--- test/test_singleton_class.rb
+++ test/test_singleton_class.rb
@@ -3,19 +3,11 @@ $VERBOSE = true
 require 'test/unit'
 
 class Foo
-
   class << self
-    
     class << self
-
-      def foo
-        :foo
-      end
-
+      def foo; end
     end
-  
   end
-  
 end
 
 class Bar < Foo; end
@@ -23,7 +15,7 @@ class Bar < Foo; end
 class Test_Singleton_Class < Test::Unit::TestCase
   
   def test_singleton_class_inherited
-    assert_same :foo, Bar.singleton_class.foo
+    assert_same nil, Bar.singleton_class.foo
   end
 
 end
